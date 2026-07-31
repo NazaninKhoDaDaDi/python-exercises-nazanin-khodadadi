620d2b4c2b323fc25be0e1bed224b9c6e6432dee
NazaninKhoDaDaDi <nnnazzii1189@gmail.com>
2026-07-31 19:08:08 +0330

    Add session 02 exercises

---
+++ session-02/question_01.py
@@ -0,0 +1,4 @@
+i = input('Enter your name and national ID together (example:Ali1234567890):')
+s = int(i[-10:])
+
+print(s)
---
+++ session-02/question_03.py
@@ -0,0 +1,2 @@
+i = input('enter your name and last name (example: Mohammad Rezaei) : ')
+print(i[0] , '.' , i[spase + 1 : ])
